67d02f24da3b6d6532cfeb371fd2f998d0bcd418
joaocastro2 <joaocastrocntt06@gmail.com>
2024-08-18 03:57:35 -0300

    sisodonto version 0.0.0.2

---
+++ script files/inserir paciente.sql
@@ -0,0 +1,9 @@
+/*
+(mudar as informações dos campos em laranja para as informações do novo paciente)
+(a data de nascimento do paciente deve ser no formato ano-mês-dia)
+(após cadastar o paciente apague a linha inteira, copia a linha a seguir e cole na linha 9)
+insert into pacientes values ('Cpf do paciente', 'Nome do paciente', 'Data de nascimento', 'sexo', 'cep', 'endereço', 'telefone', 'email');
+*/
+-- adiciona um ou mais funcionários na empresa
+insert into pacientes values ('Cpf do paciente', 'Nome do paciente', 'Data de nascimento', 'sexo', 'cep', 'endereço', 'telefone', 'email');
+
