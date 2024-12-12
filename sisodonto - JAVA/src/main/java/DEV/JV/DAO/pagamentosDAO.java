42456b800b2458eefe034d0723a2124a426de4db
joaocastro2 <joaocastrocntt06@gmail.com>
2024-12-12 10:02:46 -0300

    LISTS PAGAMENTOS OK

--- sisodonto - JAVA/src/main/java/DEV/JV/DAO/pagamentosDAO.java
+++ sisodonto - JAVA/src/main/java/DEV/JV/DAO/pagamentosDAO.java
@@ -1,11 +1,15 @@
 package DEV.JV.DAO;
 
+import DEV.JV.CLASSES_ENUM.formasPagamento;
 import DEV.JV.INFRA.ConnectionFactory;
 import DEV.JV.MODEL.pagamentosMODEL;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -45,11 +49,66 @@ public class pagamentosDAO implements IpagamentosDAO {
 
     @Override
     public List<pagamentosMODEL> findAll() {
-        return List.of();
+
+        List<pagamentosMODEL> pagamentos = new ArrayList<>();
+
+        try (Connection connection = ConnectionFactory.getConnection()) {
+
+            String sql  = "SELECT * FROM pagamentos";
+
+            PreparedStatement prepareStatement  = connection.prepareStatement(sql);
+            ResultSet  rs = prepareStatement.executeQuery();
+
+            while (rs.next()){
+                Long idPagamento =  rs.getLong("idPagamento");
+                Long fk_idConsulta = rs.getLong("fk_idConsulta");
+                LocalDate dataPagamento = rs.getDate("dataPagamento").toLocalDate();
+                Double valor = rs.getDouble("Valor");
+                formasPagamento formaPagamento =  formasPagamento.valueOf(rs.getString("Forma_Pagamento"));
+                Boolean situacao = rs.getBoolean("situação");
+
+                pagamentosMODEL pagamento = new pagamentosMODEL(idPagamento, fk_idConsulta,  dataPagamento, valor, formaPagamento, situacao);
+                pagamentos.add(pagamento);
+
+            }
+
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        return pagamentos;
     }
 
     @Override
     public Optional<pagamentosMODEL> findById(Long idPagamento) {
-        return Optional.empty();
+
+        pagamentosMODEL pagamentos  = null;
+
+        try (Connection connection  = ConnectionFactory.getConnection()){
+
+            String sql = "SELECT * FROM pagamentos WHERE idPagamento = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, idPagamento);
+
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()){
+                Long idPagamento1 =  rs.getLong("idPagamento");
+                Long fk_idConsulta = rs.getLong("fk_idConsulta");
+                LocalDate dataPagamento = rs.getDate("dataPagamento").toLocalDate();
+                Double valor = rs.getDouble("Valor");
+                formasPagamento formaPagamento =  formasPagamento.valueOf(rs.getString("Forma_Pagamento"));
+                Boolean situacao = rs.getBoolean("situação");
+
+                pagamentos = new pagamentosMODEL(idPagamento1, fk_idConsulta,  dataPagamento, valor, formaPagamento, situacao);
+
+            }
+
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        return Optional.ofNullable(pagamentos);
     }
 }
--- sisodonto - JAVA/src/main/java/DEV/JV/TEST_FUNCTIONS/LISTS/LISTS_PAGAMENTOS.java
+++ sisodonto - JAVA/src/main/java/DEV/JV/TEST_FUNCTIONS/LISTS/LISTS_PAGAMENTOS.java
@@ -1,4 +1,40 @@
 package DEV.JV.TEST_FUNCTIONS.LISTS;
 
+import DEV.JV.DAO.consultaDAO;
+import DEV.JV.DAO.pagamentosDAO;
+import DEV.JV.MODEL.consultaMODEL;
+import DEV.JV.MODEL.pagamentosMODEL;
+
+import java.util.List;
+import java.util.Optional;
+
 public class LISTS_PAGAMENTOS {
+
+    public static void main(String[] args) {
+
+        pagamentosDAO dao = new pagamentosDAO();
+        //List<pagamentosMODEL> pagamentos = dao.findAll();
+        //    for(pagamentosMODEL pagamento : pagamentos) {
+        //        System.out.println("ID DO PAGAMENTO: " + pagamento.getIdPagamento());
+        //        System.out.println("ID DA CONSULTA: " + pagamento.getFk_idConsulta());
+        //        System.out.println("DATA DO PAGAMENTO: " + pagamento.getDataPagamento());
+        //        System.out.println("VALOR: " + pagamento.getValor());
+        //        System.out.println("FORMA PAGAMENTO " + pagamento.getFormaPagamento());
+        //        System.out.println("SITUAÇÃO: " + pagamento.isSituacao());
+        //        System.out.println("==================================================");
+        //}
+
+        Optional<pagamentosMODEL> pagamentosMODELOptional = dao.findById(1L);
+
+        pagamentosMODELOptional.ifPresent(pagamentosMODEL -> {
+            System.out.println("ID DO PAGAMENTO: " + pagamentosMODEL.getIdPagamento());
+                    System.out.println("ID DA CONSULTA: " + pagamentosMODEL.getFk_idConsulta());
+                    System.out.println("DATA DO PAGAMENTO: " + pagamentosMODEL.getDataPagamento());
+                    System.out.println("VALOR: " + pagamentosMODEL.getValor());
+                    System.out.println("FORMA PAGAMENTO " + pagamentosMODEL.getFormaPagamento());
+                    System.out.println("SITUAÇÃO: " + pagamentosMODEL.isSituacao());
+        });
+
+    }
+
 }
